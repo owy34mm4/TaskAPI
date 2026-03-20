7ae06c84415314a6b781e38cb44b1e55840d2485
a <a@a.com>
2026-03-19 23:14:32 -0500

    add: bootstrap Module + InitConfigs

---
+++ src/Backend/src/bootstrap/src/main/java/com/taskapi/bootstrap/TaskAPIApplication.java
@@ -0,0 +1,13 @@
+package com.taskapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskAPIApplication.class, args);
+	}
+
+}
